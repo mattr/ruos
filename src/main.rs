67886b0dcf172582eccb0fbac199939c77a2f5cf
Matt Redmond <me@mattredmond.com>
2022-07-20 07:04:10 +0930

    WIP 4

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(ruos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-mod vga_buffer;
+use ruos::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    #[cfg(test)] test_main();
+
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    ruos::test_panic_handler(info);
 }
